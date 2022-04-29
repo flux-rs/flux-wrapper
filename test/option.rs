96c34455492255a2b6fcdc12c1ac7399e66b13b0
Ranjit Jhala <jhala@cs.ucsd.edu>
2022-04-29 10:19:37 -0700

    init flux-wrapper

---
+++ test/option.rs
@@ -0,0 +1,22 @@
+#![feature(register_tool)]
+#![register_tool(lr)]
+
+// We need this right now to not deal with the enum `Some`
+
+#[lr::assume]
+fn some<T>(x: T) -> Option<T> {
+    Option::Some(x)
+}
+
+#[lr::sig(fn(i32) -> i32{v:0 < v})]
+pub fn test2(n: i32) -> i32 {
+    if (0 < n) { 
+        let opt = some(n);
+        opt.unwrap()
+    } else {
+        0
+    }
+}
+
+
+
